f3c9e8581118997bcdf5d20f573f168229a28591
peterwilmshurst <peterwilmshurst@hotmail.com>
2021-04-16 21:08:16 +0100

    hide summary on mobile

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="portfolio"> 
     <ul>
-      <li><div class="overlay"><h2>The Dinner Table</h2><p>Beautiful gift box brownies with a purpose, delivered across the UK and straight to your door from Oxted, Surrey.</p><p class="services-text">Design | Development | Shopify</p><a href="https://www.thedinnertable.co.uk" target="_blank">Visit the website</a></div><img src="../assets/portfolio_the-dinner-table.jpg" alt="The Dinner Table"></li>
-      <li><div class="overlay"><h2>Oxted Classic Cars</h2><p>Classic car hire for weddings, self-drive, TV and film, photoshoots and any other bespoke service you may require. Our diverse collection of true classics range from 1938 to 1969.</p><p class="services-text">Design | Development | Digital Marketing | Hosting</p><a href="https://www.oxtedclassiccars.co.uk" target="_blank">Visit the website</a></div><img src="../assets/portfolio_oxted-classic-cars.jpg" alt="Oxted Classic Cars"></li>
-      <li><div class="overlay"><h2>Rafael Tome</h2><p>On a mission to help organisations deliver best in class customer experience through brands, services and products to therefore create meaningful human impact and eventually make people’s lives better.</p><p class="services-text">Development</p><a href="https://flamboyant-tereshkova-fbaf42.netlify.app/" target="_blank">Visit the website</a></div><img src="../assets/portfolio_rafael-tome.jpg" alt="Rafael Tome"></li>
-      <li><div class="overlay"><h2>Teepee Emporium</h2><p>Luxury Teepee Sleepovers and Alfresco Events. Our fabulous Teepees make any event truly unique. Let us help you create your perfect celebration, whatever the weather.</p><p class="services-text">Design | Development</p><a href="https://www.teepee-emporium.co.uk" target="_blank">Visit the website</a></div><img src="../assets/portfolio_teepee-emporium.jpg" alt="Teepee Emporium"></li>
-      <li><div class="overlay"><h2>Little Seeds</h2><p>The UK's Best Selling Swaddle Blankets and the softest most comfortable organic baby clothing with timeless and modern neutral designs.</p><p class="services-text">Design | Development | Shopify | Digital Marketing</p><a href="https://www.little-seeds.co.uk" target="_blank">Visit the website</a></div><img src="../assets/portfolio_little-seeds.jpg" alt="Little Seeds"></li>
-      <li><div class="overlay"><h2>Stonerite Memorials</h2><p>Stonerite Memorials - Bespoke Memorials and Headstones using the finest natural stone.</p><p class="services-text">Development | Wordpress</p><a href="http://www.stonerite.co.uk/" target="_blank">Visit the website</a></div><img src="../assets/portfolio_stonerite-memorials.jpg" alt="Stonerite Memorials"></li>
+      <li><div class="overlay"><h2>The Dinner Table</h2><p class="summary-text">Beautiful gift box brownies with a purpose, delivered across the UK and straight to your door from Oxted, Surrey.</p><p class="services-text">Design | Development | Shopify</p><a href="https://www.thedinnertable.co.uk" target="_blank">Visit the website</a></div><img src="../assets/portfolio_the-dinner-table.jpg" alt="The Dinner Table"></li>
+      <li><div class="overlay"><h2>Oxted Classic Cars</h2><p class="summary-text">Classic car hire for weddings, self-drive, TV and film, photoshoots and any other bespoke service you may require. Our diverse collection of true classics range from 1938 to 1969.</p><p class="services-text">Design | Development | Digital Marketing | Hosting</p><a href="https://www.oxtedclassiccars.co.uk" target="_blank">Visit the website</a></div><img src="../assets/portfolio_oxted-classic-cars.jpg" alt="Oxted Classic Cars"></li>
+      <li><div class="overlay"><h2>Rafael Tome</h2><p class="summary-text">On a mission to help organisations deliver best in class customer experience through brands, services and products to therefore create meaningful human impact and eventually make people’s lives better.</p><p class="services-text">Development</p><a href="https://flamboyant-tereshkova-fbaf42.netlify.app/" target="_blank">Visit the website</a></div><img src="../assets/portfolio_rafael-tome.jpg" alt="Rafael Tome"></li>
+      <li><div class="overlay"><h2>Teepee Emporium</h2><p class="summary-text">Luxury Teepee Sleepovers and Alfresco Events. Our fabulous Teepees make any event truly unique. Let us help you create your perfect celebration, whatever the weather.</p><p class="services-text">Design | Development</p><a href="https://www.teepee-emporium.co.uk" target="_blank">Visit the website</a></div><img src="../assets/portfolio_teepee-emporium.jpg" alt="Teepee Emporium"></li>
+      <li><div class="overlay"><h2>Little Seeds</h2><p class="summary-text">The UK's Best Selling Swaddle Blankets and the softest most comfortable organic baby clothing with timeless and modern neutral designs.</p><p class="services-text">Design | Development | Shopify | Digital Marketing</p><a href="https://www.little-seeds.co.uk" target="_blank">Visit the website</a></div><img src="../assets/portfolio_little-seeds.jpg" alt="Little Seeds"></li>
+      <li><div class="overlay"><h2>Stonerite Memorials</h2><p class="summary-text">Stonerite Memorials - Bespoke Memorials and Headstones using the finest natural stone.</p><p class="services-text">Development | Wordpress</p><a href="http://www.stonerite.co.uk/" target="_blank">Visit the website</a></div><img src="../assets/portfolio_stonerite-memorials.jpg" alt="Stonerite Memorials"></li>
     </ul>
   </div>
 </template>
@@ -57,6 +57,20 @@ $grey: #38344d;
 					padding-left: 1rem;
 					padding-right: 1rem;
 					text-align: center;
+				}
+
+				.summary-text {
+						display: none;
+						visibility: hidden;
+					}
+
+				@media only screen and (min-width: 768px) {
+				
+				.summary-text {
+						display: block;
+						visibility: visible;
+					}
+					
 				}
 
 				.services-text {
